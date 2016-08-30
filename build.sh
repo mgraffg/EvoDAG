6e86930766efacf52701660fa03040a137a21cfb
Mario Graff <mgraffg@ieee.org>
2016-08-30 07:44:06 -0500

    anaconda build

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-echo "Building"
+#!/bin/bash
+
+cd $RECIPE_DIR
+# echo "Building !!!!" `pwd`
+$PYTHON setup.py build_ext
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
