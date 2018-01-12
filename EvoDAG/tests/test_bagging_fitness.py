05a39f99033cf1ad43d097778d4eb8e1c67fc010
Mario Graff <mgraffg@ieee.org>
2018-01-11 22:04:36 -0600

    improving tests

--- EvoDAG/tests/test_bagging_fitness.py
+++ EvoDAG/tests/test_bagging_fitness.py
@@ -427,7 +427,8 @@ def test_a_precision():
     gp.y = y
     gp.X = X
     gp.create_population()
-    off = gp.random_offspring()
+    # off = gp.random_offspring()
+    off = gp.population.bsf
     hy = SparseArray.argmax(off.hy)
     index = np.array(gp._mask_ts.index)
     y = np.array(gp._y_klass.full_array())[index]
