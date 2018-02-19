1902c257140189a959bf5f6404761daf0d2f28de
Mario Graff <mgraffg@ieee.org>
2018-02-19 06:56:49 -0600

    travis update

---
+++ continuous-integration/binstar-push.py
@@ -0,0 +1,21 @@
+import os
+import glob
+import subprocess
+import traceback
+
+
+def get_token():
+    token = None
+    if os.environ.get('TRAVIS_BRANCH', None) == 'master' or os.environ.get('APPVEYOR_REPO_BRANCH', None) == 'master':
+        token = os.environ.get('BINSTAR_TOKEN', None)
+    return token
+
+
+token = get_token()
+if token is not None:
+    cmd = ['binstar', '-t', token, 'upload', '--force']
+    cmd.extend(glob.glob('*.tar.bz2'))
+    try:
+        subprocess.check_call(cmd)
+    except subprocess.CalledProcessError:
+        traceback.print_exc()
